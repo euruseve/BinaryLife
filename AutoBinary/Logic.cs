4f045c964706f25777d92d3daadbb2ed63b273cc
euruseve <ostrovskyivladislav@gmail.com>
2021-08-01 19:29:38 +0300

    adding files

---
+++ AutoBinary/Logic.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+
+namespace AutoBinary
+{
+    public class Logic
+    {
+        private int _size;
+        private bool[] _arr;
+        private bool[] _rule = new bool[8];
+        static Random random = new Random();
+
+        public Logic(int size)
+        {
+            _size = size;
+            _arr = new bool[size];
+        }
+
+        
+        public void SetRandom()
+        {
+            for (int i = 0; i < _size; i++)
+                _arr[i] = random.Next(0,2) == 1;
+        }
+        
+        public void SetMiddleOne()
+        {
+            for (int i = 0; i < _size; i++)
+                _arr[i] = false;
+            _arr[_size / 2] = true;
+        }
+
+        public void SetRule(int nr)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                _rule[i] = (nr % 2 == 1);
+                nr /= 2;
+            }
+        }
+
+        public void Next()
+        {
+            bool[] nextArr = new bool[_size];
+            for (int i = 0; i < _size; i++)
+            {
+                int cat = 4 * (_arr[(i - 1 + _size) % _size] ? 1 : 0) +
+                          2 * (_arr[i                      ] ? 1 : 0) +
+                          1 * (_arr[(i + 1) % _size        ] ? 1 : 0);
+                nextArr[i] = _rule[cat];
+            }
+
+            for (int i = 0; i < _size; i++)
+                _arr[i] = nextArr[i];
+        }
+
+        public void Change()
+        {
+            int nr = random.Next(0, _size);
+            _arr[nr] = !_arr[nr];
+        }
+        
+        public void ChangeRule()
+        {
+            int nr = random.Next(0, 8);
+            _rule[nr] = !_rule[nr];
+        }
+        public string Life()
+        {
+            StringBuilder sb = new StringBuilder(_size);
+            for (int i = 0; i < _size; i++)
+                sb.Append(_arr[i] ? "#" : ".");
+            return sb.ToString();
+        }
+        
+    }
+}
---
+++ AutoBinary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace AutoBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+        }
+
+        public void Start()
+        {
+            Logic logic = new Logic(79);
+            logic.SetRandom();
+            logic.SetRule(161);
+            int nr = 0;
+            while (true)
+            {
+                //Console.SetCursorPosition(0, 0);
+                if(nr++ % 25 == 0)
+                    logic.ChangeRule();
+                Console.WriteLine(logic.Life());
+                logic.Next();
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
